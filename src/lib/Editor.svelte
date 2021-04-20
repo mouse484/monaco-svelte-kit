946e3e737ca8468744aa7af6d387698b1704dc8e
mouse_484 <38714187+mouse484@users.noreply.github.com>
2021-04-20 08:16:53 +0000

    add worker

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -1,12 +1,32 @@
+<script lang="ts" context="module">
+	declare global {
+		interface Window {
+			MonacoEnvironment: { getWorker(moduleId: unknown, label: string): Worker };
+		}
+	}
+</script>
+
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import type { editor } from 'monaco-editor';
+	import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+	import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
 	let editor: editor.IStandaloneCodeEditor;
 	onMount(() => {
+		if (import.meta.env.SSR) return;
 		import('monaco-editor').then((monaco) => {
+			self.MonacoEnvironment = {
+				getWorker: (_moduleId, label) => {
+					if (label === 'typescript' || label === 'javascript') {
+						return new tsWorker();
+					}
+					return new editorWorker();
+				}
+			};
 			editor = monaco.editor.create(document.getElementById('monaco-editor'), {
-				language: 'typescript'
+				language: 'typescript',
+				value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n')
 			});
 		});
 	});
