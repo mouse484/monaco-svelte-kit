1a16a1c40102d2591a7e09fe185f427aa0ec3d1a
mouse_484 <38714187+mouse484@users.noreply.github.com>
2021-04-21 11:35:48 +0000

    monaco utils

---
+++ src/lib/monaco/Monaco.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Monaco } from './monaco';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		if (import.meta.env.SSR) return;
+		new Monaco();
+	});
+</script>
+
+<div id="monaco-editor" />
+
+<style>
+	#monaco-editor {
+		height: 100%;
+	}
+</style>
