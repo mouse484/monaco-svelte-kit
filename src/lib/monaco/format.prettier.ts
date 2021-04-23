42c1affd7702a0330e59196499365db8e422c06a
mouse_484 <escmouse484@gmail.com>
2021-04-23 09:39:54 +0000

    formatter

--- src/lib/monaco/format.prettier.ts
+++ src/lib/monaco/format.prettier.ts
@@ -1,6 +1,18 @@
 import * as prettier from 'prettier/standalone';
+import type { Options } from 'prettier';
 import tsParser from 'prettier/parser-typescript';
 
-export const format = (source: string) => {
-	return prettier.format(source, { parser: 'typescript', plugins: [tsParser] });
+const getParser = (
+	language: string
+): Pick<Options, 'parser'> & Pick<Options, 'plugins'> => {
+	switch (language) {
+		case 'typescript':
+			return { parser: 'typescript', plugins: [tsParser] };
+		default:
+			return {};
+	}
+};
+
+export const format = (source: string, language: string): string => {
+	return prettier.format(source, { singleQuote: true, ...getParser(language) });
 };
