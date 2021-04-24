99783ebaba08ae9df9fd829f712903b65bd7669e
mouse_484 <escmouse484@gmail.com>
2021-04-24 11:02:16 +0000

    theme

--- src/lib/monaco/monaco.ts
+++ src/lib/monaco/monaco.ts
@@ -16,13 +16,14 @@ import { format } from './format.prettier';
 
 export class Monaco {
   monaco!: monaco;
+  editor!: editor.IStandaloneCodeEditor;
   constructor() {
     import('./monaco.modules').then(({ monaco }) => {
       this.monaco = monaco;
       this.init();
-      this.addTheme('dracula');
       this.setFormatter();
       this.create();
+      this.addTheme('dracula');
     });
   }
   private init() {
@@ -30,22 +31,56 @@ export class Monaco {
       getWorker,
     };
   }
-  private create() {
-    this.monaco.editor.create(document.getElementById('monaco-editor'), {
-      language: 'typescript',
-      value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
-        '\n'
-      ),
+  private async create() {
+    this.editor = this.monaco.editor.create(
+      document.getElementById('monaco-editor'),
+      {
+        language: 'typescript',
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
+          '\n'
+        ),
+      }
+    );
+    const { loadWASM } = await import('onigasm');
+    const { Registry } = await import('monaco-textmate');
+    const { wireTmGrammars } = await import(
+      '$lib/monaco/monaco-editor-textmate'
+    );
+
+    const name = 'vs-code-theme-converted';
+    await loadWASM('./node_modules/onigasm/lib/onigasm.wasm');
+
+    const registry = new Registry({
+      getGrammarDefinition: async () => {
+        return {
+          format: 'plist',
+          content: await (
+            await fetch('./src/lib/monaco/themes/TypeScript.tmLanguage.plist')
+          ).text(),
+        };
+      },
     });
-  }
-  private addTheme(name: string) {
-    import(`./themes/dracula.json`).then((theme) => {
+    const grammars = new Map();
+    grammars.set('typescript', 'source.ts');
+
+    import(`./themes/new/dracula.json`).then((theme) => {
       this.monaco.editor.defineTheme(
         name,
         theme.default as editor.IStandaloneThemeData
       );
       this.monaco.editor.setTheme(name);
     });
+
+    await wireTmGrammars(this.monaco, registry, grammars, this.editor);
+  }
+  private async addTheme(_name: string) {
+    // import(`./themes/dracula.json`).then((theme) => {
+    //   this.monaco.editor.defineTheme(
+    //     name,
+    //     theme.default as editor.IStandaloneThemeData
+    //   );
+    //   this.monaco.editor.setTheme(name);
+    // });
   }
   private setFormatter() {
     this.monaco.languages.getLanguages().map(({ id: language }) => {
---
+++ theme.js
@@ -0,0 +1,7 @@
+// commonjs
+const {
+  convertThemeFromDir,
+} = require('monaco-vscode-textmate-theme-converter');
+
+convertThemeFromDir('./theme', './theme/out');
+
